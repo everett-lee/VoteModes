18228f85b2967e62f2508bfff1c8dfe337c1cbd9
Lee <lee.m.everett@gmail.com>
2021-05-01 13:54:11 +0100

    Start to format

--- Downloader/downloader.py
+++ Downloader/downloader.py
@@ -3,9 +3,6 @@ import json
 
 TOTAL_MPS = 650
 
-def has_good_attendance(division):
-    return division['ayes'] + division['noes'] > TOTAL_MPS * 0.8
-
 def download_divisions_list():
     params = {'queryParameters.startDate': '2020-01-01'}
     res = requests.get('https://commonsvotes-api.parliament.uk/data/divisions.json/search', params)
@@ -14,13 +11,23 @@ def download_divisions_list():
         rawJSon.write(res.text)
 
 def what_is_it():
+    def has_good_attendance(division):
+        return division['AyeCount'] + division['NoCount'] > TOTAL_MPS * 0.8
+
+    def get_fields_of_interest(division):
+        return {
+            'DivisionId': division['DivisionId'],
+            'Date': division['Date'],
+            'Title': division['Title']
+        }
+
     with open('./raw/rawDivsions', 'r') as rawJSon:
         res = rawJSon.read()
         raw = json.loads(res)
 
-        print(len(raw))
         with_good_attendance = [div for div in raw if has_good_attendance(div)]
-        print(len(with_good_attendance))
+        with_fields_of_interest = [get_fields_of_interest(div) for div in with_good_attendance]
+        print(with_fields_of_interest)
 
 
 def getit():
