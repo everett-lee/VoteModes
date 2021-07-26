3895ab81ac2e3a5070332d5b16806f259d7bb4bb
Lee <lee.m.everett@gmail.com>
2021-07-26 22:07:24 +0100

    Refactor MonthWithIntervals

--- downloader_lambda_app/request_executors/divisions/month_with_intervals.py
+++ downloader_lambda_app/request_executors/divisions/month_with_intervals.py
@@ -9,12 +9,9 @@ class DateInterval:
 
 class MonthWithIntervals:
     def __init__(self, month: int, year: int):
-        self.month = month
-        self.month_str = self.month_to_str(month)
-        self.year = year
-        self.first_interval = self.get_first_interval()
-        self.second_interval = self.get_second_interval()
-        self.third_interval = self.get_third_interval()
+        self.first_interval = self.get_first_interval(month, year)
+        self.second_interval = self.get_second_interval(month, year)
+        self.third_interval = self.get_third_interval(month, year)
         self.get_interval_list = [self.first_interval, self.second_interval, self.third_interval]
 
     @staticmethod
@@ -24,22 +21,27 @@ class MonthWithIntervals:
         else:
             return "{month}".format(month=int_month)
 
-    def get_first_interval(self) -> DateInterval:
-        first_open = '{year}-{month}-01'.format(year=self.year, month=self.month_str) if self.month == 1 \
-            else '{year}-{month}-02'.format(year=self.year, month=self.month_str)
-        first_close = '{year}-{month}-10'.format(year=self.year, month=self.month_str)
+    def get_first_interval(self, month, year) -> DateInterval:
+        month_str = self.month_to_str(month)
+        first_open = '{year}-{month}-01'.format(year=year, month=month_str) if month == 1 \
+            else '{year}-{month}-02'.format(year=year, month=month_str)
+        first_close = '{year}-{month}-10'.format(year=year, month=month_str)
 
         return DateInterval(first_open, first_close)
 
-    def get_second_interval(self) -> DateInterval:
-        second_open = '{year}-{month}-11'.format(year=self.year, month=self.month_str)
-        second_close = '{year}-{month}-20'.format(year=self.year, month=self.month_str)
+    def get_second_interval(self, month, year) -> DateInterval:
+        month_str = self.month_to_str(month)
+        second_open = '{year}-{month}-11'.format(year=year, month=month_str)
+        second_close = '{year}-{month}-20'.format(year=year, month=month_str)
 
         return DateInterval(second_open, second_close)
 
-    def get_third_interval(self) -> DateInterval:
-        third_open = '{year}-{month}-21'.format(year=self.year, month=self.month_str)
-        third_close = '{year}-{month}-01'.format(year=self.year, month=self.month_to_str(self.month + 1)) if \
-            self.month < 12 else '{year}-12-31'.format(year=self.year)
+    def get_third_interval(self, month, year) -> DateInterval:
+        month_str = self.month_to_str(month)
+        next_month_str = self.month_to_str(month + 1)
+
+        third_open = '{year}-{month}-21'.format(year=year, month=month_str)
+        third_close = '{year}-{month}-01'.format(year=year, month=next_month_str) if \
+            month < 12 else '{year}-12-31'.format(year=year)
 
         return DateInterval(third_open, third_close)
