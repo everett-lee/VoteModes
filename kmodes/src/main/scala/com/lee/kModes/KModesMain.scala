879721a75d54891ed5a9c3ef5aba4ce48847999e
Lee <lee.m.everett@gmail.com>
2022-11-30 20:17:04 +0000

    Fix typo

--- kmodes/src/main/scala/com/lee/kModes/KModesMain.scala
+++ kmodes/src/main/scala/com/lee/kModes/KModesMain.scala
@@ -32,7 +32,6 @@ class KModesMain(centroidsHelper: CentroidsHelper[Vector[MPWithVotes], List[Vote
       (centroidIndex, mpsWithVotes) <- groupedByCentroid
     } yield centroidsHelper.calculateCentroid(mpsWithVotes, centroidIndex)).toVector
     logger.info("New centroids: {}", newCentroids)
-ead
     looper(inMps, newCentroids, iteration + 1)
   }
 
